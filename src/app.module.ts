98c903aa6d524da4d7d67cfc41fe379bb2023f57
Anton Titov <titovanton555@gmail.com>
2023-07-21 00:24:20 +0600

    chore: change configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,31 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AppResponseInterceptor } from './comon/interceptors/app-response.interceptor';
+import { AppExceptionFilter } from './comon/filters/app-exception.filter';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, UserModule],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AppExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AppResponseInterceptor,
+    },
+  ],
 })
 export class AppModule {}
