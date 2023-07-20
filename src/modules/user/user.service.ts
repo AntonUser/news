3cd7cc6d6892ecdbb77e2f03d1aacb261333ed0c
Anton Titov <titovanton555@gmail.com>
2023-07-21 00:23:01 +0600

    feat: create methods for authentication

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { UserCreateDto } from './dto/user-create.dto';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  create(dto: UserCreateDto) {
+    const user = this.userRepository.create(dto);
+    return this.userRepository.save(user);
+  }
+
+  findByEmailOrFail(email: string) {
+    return this.userRepository.findOneByOrFail({ email });
+  }
+}
